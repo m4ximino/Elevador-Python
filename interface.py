90d04d0e6d2e001a0f3c5dc5ef0fca03e44af518
Max <mmaa@ic.ufal.br>
2022-07-22 16:14:56 -0300

    Projeto terminado

--- interface.py
+++ interface.py
@@ -3,12 +3,17 @@ import main
 def last():
     elevador: main.Elevador = main.Elevador(0,100)
     print('Olá bem vindo ao sistema de Elevador')
-    while input('Tem alguem esperando o elevador?') != 'sim':
-        nome = input()
-        origem = int(input())
-        destino = int(input())
+    while input('Tem alguem esperando o elevador ? ') == 'sim':
+        nome = input('Diga-me qual o nome do passageiro: ')
+        origem = int(input('Diga-me que andar ele(a) está: '))
+        destino = int(input('Para qual andar ele(a) deseja ir ? '))
         if origem >= 0 and origem <= 100 or destino <= 100 and destino >= 0:
             pessoa: main.User = main.User(nome,origem,destino)
             elevador.add(pessoa)
         else: print("O elevador não pode ir até esse andar...")
+    
+    elevador.distancia_do_elevador()
+    elevador.levar_elevador()
+        
+last()
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from typing import List
+
 subir = 1
 descer = 0
+
+# Class para criar passageiro.
 class User:
     def __init__(self, nome,origem, destino):
+        self.__id: str = nome
         self.__origem = origem
         self.__destino = destino
-        self.__id: str = nome
             
     @property
     def origem(self: object) -> int:
@@ -23,18 +26,14 @@ class User:
     def id(self: object) -> str:
         return self.__id
     
+# Class para elevador.
 class Elevador:
-    contador=1
     def __init__(self, andar, limite):       
-        self.__id = Elevador.contador
         self.__esperando: List[User]= []
-        self.__capacidade: int = 0
         self.__dentro: List[User] = []
         self.atual = andar
         self.__limite = limite
-        self.__disp = True
         self.__aux = 0
-        Elevador.contador+=1
 
     @property
     def id(self: object) -> int:
@@ -54,7 +53,8 @@ class Elevador:
     
     def add(self, pessoa:User):
         self.esperando.append(pessoa)
-            
+    
+    # Ordenando por passageiro mais próximo do andar atual do elevador
     def distancia_do_elevador(self):
         try:
             for i in range(len(self.esperando)):
@@ -65,7 +65,8 @@ class Elevador:
                         self.esperando[j] = aux
         except TypeError:
             None
-            
+    
+    # Levando elevador até o andar do passageiro       
     def levar_elevador(self):
         try:
             if self.atual == self.esperando[0].origem:
@@ -78,9 +79,9 @@ class Elevador:
             else:
                 self.move_elevador(descer)
             return self.levar_elevador()
-            
         except IndexError: None
     
+    # Levando passagiero já dentro do elevador para o andar de destino
     def levar_passageiro(self):
         try:
             if self.atual == self.dentro[0].destino:
@@ -95,6 +96,7 @@ class Elevador:
                     self.levar_passageiro()
         except RecursionError and IndexError: None
     
+    # Move o elevador de acordo a direção do elevador (subir, descer)
     def move_elevador(self, direcao: int):
         if direcao == 1:
             print(f"Elevador está no piso : {self.atual}")
@@ -104,19 +106,18 @@ class Elevador:
             print(f"Elevador está no piso : {self.atual}")
             self.vereficar_piso()
             self.atual-=1
-            
+    
+    # Verifica se tem esperando o elevador no andar ou se é o destino de algum passageiro
     def vereficar_piso(self):
         for i in self.esperando:
             if self.atual==i.origem:
                 print(f"Elevador pegou passageiro : {i.id}")
                 self.dentro.append(self.esperando[0])
                 self.esperando.remove(self.esperando[0])
-                # self.capacidade+=1
         for i in self.dentro:
             if self.atual == i.destino:
                 print(f"Elevadou chegou no destino de : {i.id}")
                 self.dentro.remove(i)
-                # self.capacidade-=1
             
             
         
